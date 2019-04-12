f3034d6d666b30558b1228211784571dba9968e8
Vera Mankongvanichkul <vmankongvanichkul@mail.greenriver.edu>
2019-04-11 23:32:39 -0400

    Add composer, base index.php, base home.html

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/** Created by PhpStorm... */
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//define a default route
+$f3->route('views/home.html');
+
+//Run fat free
+$f3->run();
+
+?>
