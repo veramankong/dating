1c4b5d95ac9f39208874a1e6173e229aea9d10c0
Vera Mankongvanichkul <vmankongvanichkul@mail.greenriver.edu>
2019-04-12 18:25:29 -0400

    Fix typo in index.php

--- index.php
+++ index.php
@@ -17,9 +17,6 @@ require_once('vendor/autoload.php');
 //Create an instance of the Base class
 $f3 = Base::instance();
 
-//fat-free error reporting
-$f3->
-
 //define a default route
 $f3->route('GET /', function() {
     $view = new Template();
