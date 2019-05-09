62a36d891b9f46cdd138c3aa686a90a52a922777
Vera Mankongvanichkul <vmankongvanichkul@mail.greenriver.edu>
2019-05-09 00:33:26 -0400

    Add Member and PremiumMember classes

---
+++ classes/PremiumMember.php
@@ -0,0 +1,40 @@
+<?php
+
+class PremiumMember
+{
+    private $_inDoorInterests;
+    private $_outDoorInterests;
+
+    /**
+     * @return mixed
+     */
+    public function getInDoorInterests()
+    {
+        return $this->_inDoorInterests;
+    }
+
+    /**
+     * @param mixed $inDoorInterests
+     */
+    public function setInDoorInterests($inDoorInterests)
+    {
+        $this->_inDoorInterests = $inDoorInterests;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOutDoorInterests()
+    {
+        return $this->_outDoorInterests;
+    }
+
+    /**
+     * @param mixed $outDoorInterests
+     */
+    public function setOutDoorInterests($outDoorInterests)
+    {
+        $this->_outDoorInterests = $outDoorInterests;
+    }
+
+}
