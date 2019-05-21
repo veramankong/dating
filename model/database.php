36fd94bf4e18891e24fc160022ae01ce51439086
Vera Mankongvanichkul <vmankongvanichkul@mail.greenriver.edu>
2019-05-21 02:45:20 -0400

    Add database class

---
+++ model/database.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+class database
+{
+    function connect()
+    {
+
+    }
+
+    function insertMembers()
+    {
+
+    }
+
+    function getMembers(member_id)
+    {
+
+    }
+
+    function getInterests(member_id)
+    {
+
+    }
+}
